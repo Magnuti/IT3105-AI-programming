b80c061ada874b2332e0ff65289f3e996b5b8004
Magnus <magnustidemann@gmail.com>
2021-03-16 14:04:48 +0100

    Added visualization for Hex board and modulized code

--- project_2/sim_world.py
+++ project_2/sim_world.py
@@ -1,4 +1,13 @@
 import numpy as np
+import networkx as nx
+
+from visualization import visualize_board
+from constants import BoardCell
+
+"""
+Player 1 = [0, 1], player 0 = [1, 0]
+Player 1 wants to maximize the reward, while player 0 wants to minimize.
+"""
 
 
 class SimWorldInterface:
@@ -18,7 +27,6 @@ class SimWorldInterface:
         """
         Returns the state of the board with the first two indices being player bits.
         """
-        # Player 1 = [0, 1], player 0 = [1, 0]
         state = [0, 1] if self.current_player else [1, 0]
         state.extend(self.state)
         return np.array(state, dtype=int)
@@ -28,17 +36,16 @@ class SimWorldInterface:
 
     def pick_move(self, next_state):
         self.state = next_state[2:]
-        self.current_player = 1 - self.current_player  # Flip between 0 and 1
 
-    def set_state_and_player(self, state):
-        raise NotImplementedError()
+    def next_player(self):
+        self.current_player = 1 - self.current_player  # Flip between 0 and 1
 
     def get_gameover_and_reward(self):
         raise NotImplementedError()
 
     def print_current_game_state(self):
         game_state = self.get_game_state()
-        # print("Current player:", game_state[0:2])
+        print("Current player:", self.current_player)
         print("Game state:\n\t", game_state)
         print("Child states:")
         for child_state in self.get_child_states():
@@ -64,7 +71,7 @@ class SimWorldNim(SimWorldInterface):
         super().__init__()
 
     def reset_game(self):
-        self.current_player = 0
+        self.current_player = 1
         self.state = np.zeros(self.N, dtype=int)
         self.state[-1] = 1
 
@@ -83,99 +90,159 @@ class SimWorldNim(SimWorldInterface):
             if i > self.__get_remaining_pieces():
                 child_states.append(None)
             else:
-                child_state = [0, 1] if not self.current_player else [1, 0]
+                child_state = [0, 1] if self.current_player else [1, 0]
                 child_state.extend(np.roll(self.state, -i))
                 child_states.append(np.array(child_state, dtype=int))
 
         return child_states
 
-    def set_state_and_player(self, state):
-        """
-        Used to reset the board to the state it was before the rollout simulation started.
-        """
-        self.current_player = state[:2]
-        self.state = state[2:]
-
     def get_gameover_and_reward(self):
         if self.__get_remaining_pieces() > 0:
             return False, 0
 
-        # Player 1 wants to maximize, while player 0 wants to minimize
         reward = 1 if self.current_player else -1
         return True, reward
 
 
 class SimWorldHex(SimWorldInterface):
+    class Cell:
+        """
+        Used to build the graph of the game state which can be passed to the
+        networkx library for visual representations.
+        """
+
+        def __str__(self):
+            return 'Cell_' + str(self.pos)
+
+        def __repr__(self):
+            return 'Cell_' + str(self.pos)
+
+        def __init__(self, status, pos):
+            self.status = status
+            self.neighbor_indices = []
+            self.pos = pos
+
     def __init__(self, board_size):
         """
         Args
             board_size: int
         """
         self.board_size = board_size
-        self.neighbor_indices_list = self.__init_neighbor_indices(
+        self.neighbor_indices_list, self.cells = self.__init_neighbor_indices(
             self.board_size)
         # Each player has a list of disjoint sets. When A and B are in the
         # same set we have a winner.
-        # self.neighbor_sets = {0: [{"A"}, {"B"}], 1: [{"A"}, {"B"}]}
-        self.neighbor_sets = {0: [{"R1"}, {"R2"}], 1: [{"L1"}, {"L2"}]}
         super().__init__()
+        # Player 0 is red (R1 and R2), while player 1 is black (B1 and B2)
+        self.neighbor_sets = {0: [{"R1"}, {"R2"}], 1: [{"B1"}, {"B2"}]}
+        self.graph = self.__init_graph(self.cells)
 
     def __init_neighbor_indices(self, board_size):
+        cells = []
+
         neighbor_indices_list = []
         cell_count = board_size**2
 
         for i in range(cell_count):
-            current_row = i // board_size
-            neighbor_indices = []
+            cell = self.Cell(status=BoardCell.EMPTY_CELL,
+                             pos=self.__index_to_coordinate(i, board_size))
+            current_row = cell.pos[0]
+            neighbor_indices = []  # Holds R1, R2, L1, L2 as well
 
             # Top
             k = i - board_size
             if(k >= 0):
                 neighbor_indices.append(k)
+                cell.neighbor_indices.append(k)
             else:
-                neighbor_indices.append("L1")
+                neighbor_indices.append("R1")
+                cell.neighbor_indices.append(None)
 
             # Top-right
             k = i - board_size + 1
             if(k >= 0 and (k // board_size) == current_row - 1):
                 neighbor_indices.append(k)
+                cell.neighbor_indices.append(k)
             else:
                 neighbor_indices.append(None)
+                cell.neighbor_indices.append(None)
 
             # Left
             k = i - 1
             if(k >= 0 and (k // board_size) == current_row):
                 neighbor_indices.append(k)
+                cell.neighbor_indices.append(k)
             else:
-                neighbor_indices.append("R1")
+                neighbor_indices.append("B1")
+                cell.neighbor_indices.append(None)
 
             # Right
             k = i + 1
             if(k < cell_count and (k // board_size) == current_row):
                 neighbor_indices.append(k)
+                cell.neighbor_indices.append(k)
             else:
-                neighbor_indices.append("R2")
+                neighbor_indices.append("B2")
+                cell.neighbor_indices.append(None)
 
             # Bottom-left
             k = i + board_size - 1
             if(k < cell_count and (k // board_size) == current_row + 1):
                 neighbor_indices.append(k)
+                cell.neighbor_indices.append(k)
             else:
                 neighbor_indices.append(None)
+                cell.neighbor_indices.append(None)
 
             # Bottom
             k = i + board_size
             if(k < cell_count):
                 neighbor_indices.append(k)
+                cell.neighbor_indices.append(k)
             else:
-                neighbor_indices.append("L2")
+                neighbor_indices.append("R2")
+                cell.neighbor_indices.append(None)
 
             neighbor_indices_list.append(neighbor_indices)
+            cells.append(cell)
 
-        return neighbor_indices_list
+        return neighbor_indices_list, cells
+
+    def __init_graph(self, cells):
+        G = nx.empty_graph(len(cells))
+
+        # Add all edges and connect the Cell to the graph_node
+        for i, cell in enumerate(cells):
+            G.nodes[i]['data'] = cell
+            for neighbor_index in cell.neighbor_indices:
+                if neighbor_index is not None:
+                    G.add_edge(i, neighbor_index)
+
+        # Set node-positions for plotting
+        plot_pos_dict = {}
+        for node_key in G.nodes():
+            cell = G.nodes[node_key]['data']
+            # This line is inspired by the TA Mathias
+            plot_pos_dict[node_key] = (-cell.pos[0] + cell.pos[1], -
+                                       cell.pos[0] - cell.pos[1])
+
+        # Store the plot_pos_dict on the graph-object
+        G.graph['plot_pos_dict'] = plot_pos_dict
+
+        return G
+
+    def __index_to_coordinate(self, index, board_size):
+        count = 0
+        for y in range(board_size):
+            for x in range(board_size):
+                if index == count:
+                    return (y, x)
+                count += 1
+        raise IndexError(
+            f"index '{index}' is not within the bounds given by board_size")
 
     def reset_game(self):
-        self.current_player = 0
+        self.current_player = 1
         # Each cell is represented as two bits [0, 0] = empty, [1, 0] = filled by
         # player 0, and [0, 1] = filled by player 1
         self.state = np.zeros(self.board_size ** 2 * 2, dtype=int)
@@ -189,7 +256,7 @@ class SimWorldHex(SimWorldInterface):
         """
         child_states = []
         player_cells = np.array(
-            [0, 1]) if not self.current_player else np.array([1, 0], dtype=int)
+            [0, 1]) if self.current_player else np.array([1, 0], dtype=int)
         for i in range(0, len(self.state), 2):
             if np.array_equal(self.state[i: i + 2], np.array([0, 0], dtype=int)):
                 # Empty cell
@@ -216,54 +283,77 @@ class SimWorldHex(SimWorldInterface):
                 cell_index = i // 2
                 break
 
-        super().pick_move(next_state)
-        player_cached = 1 - self.current_player  # Flip between 0 and 1
-
         to_merge = [{cell_index}]
-        self.neighbor_sets[player_cached].append(to_merge[0])
+        self.neighbor_sets[self.current_player].append(to_merge[0])
         for neighbour_index in self.neighbor_indices_list[cell_index]:
-            for neighbor_set in self.neighbor_sets[player_cached]:
+            for neighbor_set in self.neighbor_sets[self.current_player]:
                 if neighbour_index in neighbor_set:
                     neighbor_set.add(cell_index)
                     to_merge.append(neighbor_set)
 
         new_set = set()
-        for i in range(len(to_merge)):
-            new_set = new_set.union(to_merge[i])
+        for merge_set in to_merge:
+            new_set = new_set.union(merge_set)
 
         for neighbor_set in to_merge:
             # The if statement avoids duplicate removes which happens if
             # the inserted cell has more than one neighbour.
-            if neighbor_set in self.neighbor_sets[player_cached]:
-                self.neighbor_sets[player_cached].remove(neighbor_set)
+            if neighbor_set in self.neighbor_sets[self.current_player]:
+                self.neighbor_sets[self.current_player].remove(neighbor_set)
 
-        self.neighbor_sets[player_cached].append(new_set)
+        self.neighbor_sets[self.current_player].append(new_set)
 
-    def set_state_and_player(self, state):
-        raise NotImplementedError()
+        super().pick_move(next_state)
 
     def get_gameover_and_reward(self):
-        player_cached = 1 - self.current_player  # Flip between 0 and 1
-
-        print(self.neighbor_sets[player_cached])
-
-        for neighbor_set in self.neighbor_sets[player_cached]:
-            # TODO look over the rewards 1 vs -1 depending on which player is on which side
-            if "R1" in neighbor_set and "R2" in neighbor_set:
-                return True, 1
-            elif "L1" in neighbor_set and "L2" in neighbor_set:
-                return True, -1
+        if self.current_player:
+            for neighbor_set in self.neighbor_sets[self.current_player]:
+                if "B1" in neighbor_set and "B2" in neighbor_set:
+                    self.winner_set = neighbor_set
+                    # Black wins, player 1
+                    return True, 1
+        else:
+            for neighbor_set in self.neighbor_sets[self.current_player]:
+                if "R1" in neighbor_set and "R2" in neighbor_set:
+                    self.winner_set = neighbor_set
+                    # Red wins, player 0
+                    return True, -1
 
         return False, 0
+
+    def update_graph_statuses(self):
+        for i in range(0, len(self.state), 2):
+            cell_index = i // 2
+            cell_state = self.state[i: i + 2]
+            if np.array_equal(cell_state, np.array([0, 0], dtype=int)):
+                # Empty cell
+                status = BoardCell.EMPTY_CELL
+            elif np.array_equal(cell_state, np.array([1, 0], dtype=int)):
+                # Player 0's cell
+                if cell_index in self.winner_set:
+                    status = BoardCell.PLAYER_0_CELL_PART_OF_WINNING_PATH
+                else:
+                    status = BoardCell.PLAYER_0_CELL
+            elif np.array_equal(cell_state, np.array([0, 1], dtype=int)):
+                # Player 1's cell
+                if cell_index in self.winner_set:
+                    status = BoardCell.PLAYER_1_CELL_PART_OF_WINNING_PATH
+                else:
+                    status = BoardCell.PLAYER_1_CELL
+            else:
+                raise ValueError("Illegal value in self.state", self.state)
+
+            self.cells[cell_index].status = status
 
 
 if __name__ == "__main__":
     # sim_world = SimWorldNim(10, 5)
-    sim_world = SimWorldHex(3)
+    sim_world = SimWorldHex(10)
 
     gameover, reward = sim_world.get_gameover_and_reward()
     while not gameover:
-        sim_world.print_current_game_state()
+        # sim_world.print_current_game_state()
+        sim_world.next_player()
         child_states = sim_world.get_child_states()
         legal_child_states = []
         for i in range(len(child_states)):
@@ -274,4 +364,12 @@ if __name__ == "__main__":
         sim_world.pick_move(legal_child_states[move_index])
         gameover, reward = sim_world.get_gameover_and_reward()
 
-    sim_world.print_current_game_state()
+    # sim_world.print_current_game_state()
+    if reward == 1:
+        print("Player 1 (black) wins")
+    else:
+        print("Player 0 (red) wins")
+
+    sim_world.update_graph_statuses()
+    visualize_board(sim_world.graph, list(
+        map(lambda x: x.status, sim_world.cells)), 0)
---
+++ project_2/visualization.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+from constants import BoardCell
+
+node_fillcolors = {
+    BoardCell.EMPTY_CELL: "white",
+    BoardCell.PLAYER_0_CELL_PART_OF_WINNING_PATH: "red",
+    BoardCell.PLAYER_0_CELL: "red",
+    BoardCell.PLAYER_1_CELL_PART_OF_WINNING_PATH: "black",
+    BoardCell.PLAYER_1_CELL: "black"
+}
+node_edgecolors = {
+    BoardCell.EMPTY_CELL: "black",
+    BoardCell.PLAYER_0_CELL_PART_OF_WINNING_PATH: "green",
+    BoardCell.PLAYER_0_CELL: "red",
+    BoardCell.PLAYER_1_CELL_PART_OF_WINNING_PATH: "green",
+    BoardCell.PLAYER_1_CELL: "black"
+}
+
+
+def visualize_board(graph, state_status_list, episode):
+    """
+    Args:
+        graph: networkx graph
+        state_status_list: list of BoardCell enums
+    """
+
+    fillcolor_map = list(map(lambda x: node_fillcolors[x], state_status_list))
+    edgecolor_map = list(map(lambda x: node_edgecolors[x], state_status_list))
+
+    plt.clf()
+    # plt.title(f'Episode {episode}')
+    nx.draw(graph, pos=graph.graph['plot_pos_dict'],
+            with_labels=False, node_color=fillcolor_map, edgecolors=edgecolor_map, linewidths=3.0)
+    plt.show(block=False)
+    plt.pause(0.001)
+    plt.show()
